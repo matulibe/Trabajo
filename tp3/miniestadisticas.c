651fd351a3f56d31d4ff41ab0aeebea04213f481
matulibe <57047770+matulibe@users.noreply.github.com>
2019-11-21 12:32:50 -0300

    Add files via upload

---
+++ tp3/miniestadisticas.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct enano {
+char nombre[MAX_NOMBRE ];
+int edad;
+int cantidad_misiones ;
+int id_rango;
+} enano_t;
+
+typedef struct rango {
+int id;
+char nombre[MAX_RANGO]
+char descripcion[MAX_DESCRIPCION ];
+} rango_t
+
+
+
+
+
+
+
+
+int main(int argc, char const *argv[]) {
+
+
+  return 0;
+}
+
+
+
+
+
+//gcc *.c -o miniestadísticas -std=c99 -Wall -Werror -Wconversion
